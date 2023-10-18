ae0932550632e64a46024ecd26b9c48447568794
Charlatan2k <octavianmihai.calin@gmail.com>
2023-10-18 16:42:08 +0200

    Agregar nueva prueba cambiando autor

--- Typescript/bookTest.ts
+++ Typescript/bookTest.ts
@@ -2,4 +2,6 @@ import { Book } from "./book";
 
 let book1 = new Book("Intro a js", 220, "233444-BC2444", "Pepe", "Edicion especial");
 
+book1.setAuthor("Carlos");
+
 console.log(book1.toString());
