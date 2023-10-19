687df8567e058f8e2469a0cbf077d82020cc6f40
Charlatan2k <octavianmihai.calin@gmail.com>
2023-10-19 17:38:12 +0200

    Ultimos retoques

--- Typescript/personTest.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var person_1 = require("./person");
-var persona1 = new person_1.Person("David", 18, "Calle Juan");
-console.log(persona1.yearOfBirth(2023));
